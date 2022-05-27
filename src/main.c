03c970e071c8e875299d877a03df0bc980231080
Jonathan Lopez <jonathanglopez@gmail.com>
2022-05-27 09:22:11 -0400

    Refactored main method

--- src/main.c
+++ src/main.c
@@ -68,17 +68,25 @@ void close_input_buffer(InputBuffer *input_buffer) {
   free(input_buffer);
 }
 
+/**
+ * evaluate_buffer:
+ *
+ * Evaluate user query
+ */
+void evaluate_buffer(InputBuffer *input_buffer) {
+  if (strcmp(input_buffer->buffer, ".exit") == 0) {
+    close_input_buffer(input_buffer);
+    exit(EXIT_SUCCESS);
+  } else {
+    printf("Unrecognized command '%s'.\n", input_buffer->buffer);
+  }
+}
+
 int main() {
   InputBuffer *input_buffer = new_input_buffer();
   while (true) {
     print_prompt();
     read_input(input_buffer);
-
-    if (strcmp(input_buffer->buffer, ".exit") == 0) {
-      close_input_buffer(input_buffer);
-      exit(EXIT_SUCCESS);
-    } else {
-      printf("Unrecognized command '%s'.\n", input_buffer->buffer);
-    }
+    evaluate_buffer(input_buffer);
   }
 }
